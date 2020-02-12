b36b1c897f2777cee024806dc72b3790c4cc4385
Ade <adekunle.ogunade@rotamaster.co.uk>
2020-02-12 18:36:01 +0000

    card

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,21 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+  "presets": [
+    ["@babel/preset-env", {
+      "modules": false
+    }]
+  ],
+  "env": {
+    "test": {
+      "presets": [
+        ["@babel/preset-env", {
+          "targets": {
+            "browsers": ["last 2 versions", "safari >= 7"],
+            node: 'current'
+          }
+        }]
+      ],
+      "plugins": ["@babel/plugin-transform-runtime"]
+    }
+  },
+  "plugins": ["@babel/plugin-syntax-dynamic-import", "transform-dynamic-import"]
 }
